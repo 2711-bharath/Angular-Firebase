397ca2b79b59dd49b2bacc67ba1d6a178a840656
2711-bharath <2711bharath@gmail.com>
2021-05-19 19:06:55 +0530

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCVCqNrm2gnNnraX1DkY70Y_GORpMBoemA",
-  authDomain: "angularfirebasefriend.firebaseapp.com",
-  projectId: "angularfirebasefriend",
-  storageBucket: "angularfirebasefriend.appspot.com",
-  messagingSenderId: "194769161728",
-  appId: "1:194769161728:web:647bedd0d63d3c9c3b52e5",
-  measurementId: "G-ZGKKPEGV01"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "XXXXXXXXXXXXXXXXXXXXXXXX"
   }
 };
 
